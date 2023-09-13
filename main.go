b115aa2df80f577f99ce8a6a736fd9b7eb65a959
irreleph4nt <29428359+irreleph4nt@users.noreply.github.com>
2023-09-14 00:48:33 +0200

    implement solver

--- main.go
+++ main.go
@@ -1,146 +1,413 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
-
-	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	"k8s.io/client-go/rest"
+	"strings"
+	"time"
 
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/cmd"
+	"github.com/cert-manager/cert-manager/pkg/issuer/acme/dns/util"
+	"go.uber.org/zap"
+	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 var GroupName = os.Getenv("GROUP_NAME")
+var zapLogger, _ = zap.NewProduction()
 
 func main() {
 	if GroupName == "" {
 		panic("GROUP_NAME must be specified")
 	}
 
-	// This will register our custom DNS provider with the webhook serving
-	// library, making it available as an API under the provided GroupName.
-	// You can register multiple DNS provider implementations with a single
-	// webhook, where the Name() method will be used to disambiguate between
-	// the different implementations.
 	cmd.RunWebhookServer(GroupName,
-		&customDNSProviderSolver{},
+		&desechttpDNSProviderSolver{},
 	)
 }
 
-// customDNSProviderSolver implements the provider-specific logic needed to
-// 'present' an ACME challenge TXT record for your own DNS provider.
-// To do so, it must implement the `github.com/cert-manager/cert-manager/pkg/acme/webhook.Solver`
-// interface.
-type customDNSProviderSolver struct {
-	// If a Kubernetes 'clientset' is needed, you must:
-	// 1. uncomment the additional `client` field in this structure below
-	// 2. uncomment the "k8s.io/client-go/kubernetes" import at the top of the file
-	// 3. uncomment the relevant code in the Initialize method below
-	// 4. ensure your webhook's service account has the required RBAC role
-	//    assigned to it for interacting with the Kubernetes APIs you need.
-	//client kubernetes.Clientset
+type desechttpDNSProviderSolver struct {
+	client *kubernetes.Clientset
 }
 
-// customDNSProviderConfig is a structure that is used to decode into when
-// solving a DNS01 challenge.
-// This information is provided by cert-manager, and may be a reference to
-// additional configuration that's needed to solve the challenge for this
-// particular certificate or issuer.
-// This typically includes references to Secret resources containing DNS
-// provider credentials, in cases where a 'multi-tenant' DNS solver is being
-// created.
-// If you do *not* require per-issuer or per-certificate configuration to be
-// provided to your webhook, you can skip decoding altogether in favour of
-// using CLI flags or similar to provide configuration.
-// You should not include sensitive information here. If credentials need to
-// be used by your provider here, you should reference a Kubernetes Secret
-// resource and fetch these credentials using a Kubernetes clientset.
-type customDNSProviderConfig struct {
-	// Change the two fields below according to the format of the configuration
-	// to be decoded.
-	// These fields will be set by users in the
-	// `issuer.spec.acme.dns01.providers.webhook.config` field.
-
-	//Email           string `json:"email"`
-	//APIKeySecretRef v1alpha1.SecretKeySelector `json:"apiKeySecretRef"`
+type desechttpDNSProviderConfig struct {
+	ApiUrl       string `json:"apiUrl"`
+	DomainName   string `json:"domainName"`
+	SecretRef    string `json:"secretName"`
+	SecretKeyRef string `json:"secretKeyName"`
 }
 
-// Name is used as the name for this DNS solver when referencing it on the ACME
-// Issuer resource.
-// This should be unique **within the group name**, i.e. you can have two
-// solvers configured with the same Name() **so long as they do not co-exist
-// within a single webhook deployment**.
-// For example, `cloudflare` may be used as the name of a solver.
-func (c *customDNSProviderSolver) Name() string {
-	return "my-custom-solver"
+type Config struct {
+	ApiKey, DomainName, ApiUrl string
 }
 
-// Present is responsible for actually presenting the DNS record with the
-// DNS provider.
-// This method should tolerate being called multiple times with the same value.
-// cert-manager itself will later perform a self check to ensure that the
-// solver has correctly configured the DNS provider.
-func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
-	cfg, err := loadConfig(ch.Config)
+type RRSet struct {
+	Name    string     `json:"name,omitempty"`
+	Domain  string     `json:"domain,omitempty"`
+	SubName string     `json:"subname,omitempty"`
+	Type    string     `json:"type,omitempty"`
+	Records []string   `json:"records"`
+	TTL     int        `json:"ttl,omitempty"`
+	Created *time.Time `json:"created,omitempty"`
+}
+
+func (c *desechttpDNSProviderSolver) Name() string {
+	return "desec-http"
+}
+
+func (c *desechttpDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
+	slogger := zapLogger.Sugar()
+	slogger.Infof("call function Present: namespace=%s, zone=%s, fqdn=%s", ch.ResourceNamespace, ch.ResolvedZone, ch.ResolvedFQDN)
+
+	config, err := clientConfig(c, ch)
+	if err != nil {
+		return fmt.Errorf("unable to get secret `%s`; %v", ch.ResourceNamespace, err)
+	}
+
+	addTxtRecord(config, ch)
+	slogger.Infof("Presented txt record %v", ch.ResolvedFQDN)
+	return nil
+}
+
+func (c *desechttpDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
+	slogger := zapLogger.Sugar()
+	slogger.Infof("call function CleanUp: namespace=%s, zone=%s, fqdn=%s", ch.ResourceNamespace, ch.ResolvedZone, ch.ResolvedFQDN)
+
+	config, err := clientConfig(c, ch)
+	if err != nil {
+		return fmt.Errorf("unable to get secret `%s`; %v", ch.ResourceNamespace, err)
+	}
+
+	removeTxtRecord(config, ch)
+	slogger.Infof("Cleaned up txt record %v", ch.ResolvedFQDN)
+	return nil
+}
+
+func (c *desechttpDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
+	slogger := zapLogger.Sugar()
+
+	k8sClient, err := kubernetes.NewForConfig(kubeClientConfig)
+	slogger.Infof("Input variable stopCh is %d length", len(stopCh))
 	if err != nil {
 		return err
 	}
 
-	// TODO: do something more useful with the decoded configuration
-	fmt.Printf("Decoded configuration %v", cfg)
-
-	// TODO: add code that sets a record in the DNS provider's console
+	c.client = k8sClient
 	return nil
 }
 
-// CleanUp should delete the relevant TXT record from the DNS provider console.
-// If multiple TXT records exist with the same record name (e.g.
-// _acme-challenge.example.com) then **only** the record with the same `key`
-// value provided on the ChallengeRequest should be cleaned up.
-// This is in order to facilitate multiple DNS validations for the same domain
-// concurrently.
-func (c *customDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
-	// TODO: add code that deletes a record from the DNS provider's console
-	return nil
+// Helpers -------------------------------------------------------------------
+func addTxtRecord(config Config, ch *v1alpha1.ChallengeRequest) {
+	/*
+		Get TXT RRSet for subname
+		RRSet exists?
+			No
+				POST: Create RRSet for subname with TXT record of ch.Key
+			Yes
+				Grab existing records and add ch.Key
+				PUT: Update RRSet TXT records with new array
+
+	*/
+	
+	var url string
+	var content string
+	var contentArr []string
+	var httpMethod string
+	var records *RRSet
+
+	slogger := zapLogger.Sugar()
+
+	// Get the subdomain portion of fqdn
+	fqdn := util.UnFqdn(ch.ResolvedFQDN)
+	subName := fqdn[:len(fqdn)-len(config.DomainName)-1]
+
+	records, err := getTxtRecords(config, ch)
+	if err != nil {
+		slogger.Error(err)
+	}
+
+	// no TXT record exists (GET returned 404), so we will create one
+	if records == nil && err == nil {
+		slogger.Infof("====== getTxtRecords: 404 (no TXT found, now creating) ======")
+		
+		content = "\\\"" + ch.Key + "\\\""
+		httpMethod = "POST"
+		url = fmt.Sprintf("%s/domains/%s/rrsets/", config.ApiUrl, config.DomainName)
+	}
+
+	// at least one TXT record exists (GET returned 200), so we will append our key as a new record
+	if records != nil && err == nil {
+		slogger.Infof("====== getTxtRecords: 200 (TXT record found, now updating) ======")
+		
+		// deSEC returns TXT values quoted, so we have to remove leading and trailing double-quotes for each value one at a time
+		for _, x := range records.Records {
+			contentArr = append(contentArr, strings.Trim(x,"\""))
+		}
+
+		contentArr = append(contentArr, ch.Key)
+
+		content = "\\\"" + strings.Join(contentArr, "\\\"\",\"\\\"") + "\\\""
+		httpMethod = "PUT"
+		url = fmt.Sprintf("%s/domains/%s/rrsets/%s.../TXT/", config.ApiUrl, config.DomainName, subName)
+	}
+
+	var jsonStr = fmt.Sprintf(`
+		{
+			"subname": "%s",
+			"type": "TXT",
+			"ttl": 3600,
+			"records": ["%s"]
+		}`, subName, content)
+
+	slogger.Infof("JSON payload is: %s", string(jsonStr))
+
+	add, _, err := callDnsApi(url, httpMethod, bytes.NewBuffer([]byte(jsonStr)), config)
+	if err != nil {
+		slogger.Error(err)
+	}
+
+	slogger.Infof("Added TXT record result: %s", string(add))
 }
 
-// Initialize will be called when the webhook first starts.
-// This method can be used to instantiate the webhook, i.e. initialising
-// connections or warming up caches.
-// Typically, the kubeClientConfig parameter is used to build a Kubernetes
-// client that can be used to fetch resources from the Kubernetes API, e.g.
-// Secret resources containing credentials used to authenticate with DNS
-// provider accounts.
-// The stopCh can be used to handle early termination of the webhook, in cases
-// where a SIGTERM or similar signal is sent to the webhook process.
-func (c *customDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
-	///// UNCOMMENT THE BELOW CODE TO MAKE A KUBERNETES CLIENTSET AVAILABLE TO
-	///// YOUR CUSTOM DNS PROVIDER
+func removeTxtRecord(config Config, ch *v1alpha1.ChallengeRequest) {
+	/*
+		Get TXT RRSet for subname
+		RRSet exists?
+			No
+				return nil (silent pass - do nothing)
+			Yes
+				Calculate existing records and remove ch.Key from array
+				Array is now empty?
+				Yes
+					DELETE: subname RRSet
+				No 
+					PUT: Update RRSet TXT records with new array
 
-	//cl, err := kubernetes.NewForConfig(kubeClientConfig)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//c.client = cl
+	*/
 
-	///// END OF CODE TO MAKE KUBERNETES CLIENTSET AVAILABLE
-	return nil
+	var url string
+	var content string
+	var contentArr []string
+	var httpMethod string
+	var records *RRSet
+
+	slogger := zapLogger.Sugar()
+
+	// Get the subdomain portion of fqdn
+	fqdn := util.UnFqdn(ch.ResolvedFQDN)
+	subName := fqdn[:len(fqdn)-len(config.DomainName)-1]
+
+	records, err := getTxtRecords(config, ch)
+	if err != nil {
+		slogger.Error(err)
+	}
+
+	// no TXT record exists (GET returned 404), so we silently skip execution (nothing to do)
+	if records == nil && err == nil {
+		slogger.Infof("====== getTxtRecords: 404 (no TXT found, skipping) ======")
+		
+		content = ""
+		httpMethod = ""
+		url = ""
+	}
+
+	// at least one TXT record exists (GET returned 200), so we will scan for our key and remove it (or delete the entire record)
+	if records != nil && err == nil {
+		slogger.Infof("====== getTxtRecords: 200 (TXT record found) ======")
+
+		// deSEC returns TXT values quoted, so we have to remove leading and trailing double-quotes for each value one at a time
+		for _, x := range records.Records {
+			contentArr = append(contentArr, strings.Trim(x,"\""))
+		}
+
+		var contentArrAmend []string
+		// Create a new records slice containing all records except for the one to be deleted
+		for _, r := range contentArr {
+			if r != ch.Key {
+				contentArrAmend  = append(contentArrAmend , r)
+			}
+		}
+
+		if len(contentArrAmend) >= 1 {
+
+			slogger.Infof("====== removeTxtRecord: Updating records ======")
+
+			content = "\"\\\"" + strings.Join(contentArrAmend, "\\\"\",\"\\\"") + "\\\"\""
+			httpMethod = "PUT"
+
+		} else {
+
+			slogger.Infof("====== removeTxtRecord: Deleting TXT for subname ======")
+
+			content = ""
+			httpMethod = "DELETE"
+
+		}
+
+		url = fmt.Sprintf("%s/domains/%s/rrsets/%s.../TXT/", config.ApiUrl, config.DomainName, subName)
+
+	}
+
+	if (httpMethod != "") && (url != "") {
+
+		var jsonStr = fmt.Sprintf(`
+		{
+			"subname": "%s",
+			"type": "TXT",
+			"ttl": 3600,
+			"records": [%s]
+		}`, subName, content)
+
+		slogger.Infof("JSON payload is: %s", string(jsonStr))
+
+		remove, _, err := callDnsApi(url, httpMethod, bytes.NewBuffer([]byte(jsonStr)), config)
+		if err != nil {
+			slogger.Error(err)
+		}
+
+		slogger.Infof("Removed TXT record result: %s", string(remove))
+
+	}
+
 }
 
-// loadConfig is a small helper function that decodes JSON configuration into
-// the typed config struct.
-func loadConfig(cfgJSON *extapi.JSON) (customDNSProviderConfig, error) {
-	cfg := customDNSProviderConfig{}
+// Config ------------------------------------------------------
+func stringFromSecretData(secretData *map[string][]byte, key string) (string, error) {
+	data, ok := (*secretData)[key]
+	if !ok {
+		return "", fmt.Errorf("key %q not found in secret data", key)
+	}
+
+	return string(data), nil
+}
+
+func loadConfig(cfgJSON *extapi.JSON) (desechttpDNSProviderConfig, error) {
+	cfg := desechttpDNSProviderConfig{}
+
 	// handle the 'base case' where no configuration has been provided
 	if cfgJSON == nil {
 		return cfg, nil
 	}
+
 	if err := json.Unmarshal(cfgJSON.Raw, &cfg); err != nil {
 		return cfg, fmt.Errorf("error decoding solver config: %v", err)
 	}
-
 	return cfg, nil
+}
+
+func clientConfig(c *desechttpDNSProviderSolver, ch *v1alpha1.ChallengeRequest) (Config, error) {
+	var config Config
+
+	cfg, err := loadConfig(ch.Config)
+	if err != nil {
+		return config, err
+	}
+
+	config.ApiUrl = cfg.ApiUrl
+	config.DomainName = cfg.DomainName
+	secretName := cfg.SecretRef
+	secretKeyName := cfg.SecretKeyRef
+
+	sec, err := c.client.CoreV1().Secrets(ch.ResourceNamespace).Get(context.TODO(), secretName, metav1.GetOptions{})
+	if err != nil {
+		return config, fmt.Errorf("unable to get secret `%s/%s`; %v", secretName, ch.ResourceNamespace, err)
+	}
+
+	apiKey, err := stringFromSecretData(&sec.Data, secretKeyName)
+	config.ApiKey = apiKey
+	if err != nil {
+		return config, fmt.Errorf("unable to get api-key from secret `%s/%s`; %v", secretName, ch.ResourceNamespace, err)
+	}
+
+	return config, nil
+}
+
+// REST Request ------------------------------------------------------
+func callDnsApi(url string, method string, body io.Reader, config Config) ([]byte, int, error) {
+	slogger := zapLogger.Sugar()
+
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return []byte{}, -1, fmt.Errorf("unable to execute request %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Token "+config.ApiKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, -1, err
+	}
+
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			slogger.Fatal(err)
+		}
+	}()
+
+	respBody, _ := io.ReadAll(resp.Body)
+
+	var statusOK bool
+
+	if method == "GET" {
+		statusOK = (resp.StatusCode >= 200 && resp.StatusCode < 300) || (resp.StatusCode == 404)
+	} else {
+		statusOK = resp.StatusCode >= 200 && resp.StatusCode < 300
+	}
+	
+	if statusOK {
+		return respBody, resp.StatusCode, nil
+	}
+
+	text := "Error in callDnsApi:" + resp.Status + " url: " + url + " method: " + method + "body: " + string(respBody)
+	slogger.Error(text)
+	return nil, -1, errors.New(text)
+}
+
+func getTxtRecords(config Config, ch *v1alpha1.ChallengeRequest) (*RRSet, error) {
+	
+	slogger := zapLogger.Sugar()
+
+	// Get the subdomain portion of fqdn
+	fqdn := util.UnFqdn(ch.ResolvedFQDN)
+	subName := fqdn[:len(fqdn)-len(config.DomainName)-1]
+	
+	url := fmt.Sprintf("%s/domains/%s/rrsets/%s.../TXT/", config.ApiUrl, config.DomainName, subName)
+
+	var jsonStr = fmt.Sprintf(`{}`)
+
+	get, statusCode, err := callDnsApi(url, "GET", bytes.NewBuffer([]byte(jsonStr)), config)
+	if err != nil {
+		return nil, fmt.Errorf("callDnsApi failed: %w", err)
+	}
+	
+	var rrset RRSet
+
+	if statusCode == 200 {
+		
+ 		err := json.Unmarshal(get, &rrset)
+		if err == nil {
+			return &rrset, nil
+		}
+	}
+
+	if statusCode == 404 {
+		return nil, nil
+	}
+
+	text := "Error in getTxtRecords:" + fmt.Sprint(statusCode) + " url: " + url + "body: " + string(get)
+	slogger.Error(text)
+	return nil, errors.New(text)
+
 }
